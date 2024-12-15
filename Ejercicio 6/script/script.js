9ca1acf3699d0cd040cf3d5fa57722c9a00f50e7
CesarPetit21480 <cesar_petit@hotmail.com>
2024-12-15 13:41:12 -0300

    Refactor

---
+++ Ejercicio 6/script/script.js
@@ -0,0 +1,23 @@
+/*6- Realiza un script que escriba una pirámide del 1 al 30 de la siguiente forma :
+
+1
+22
+333
+4444
+55555
+666666
+*/
+let cont = ""
+let accumulador = "";
+for (let i = 1; i <= 30; i++) {
+  cont = "";
+  for (let j = 0; j < i; j++) {
+    cont += (i.toString());   
+  }
+  accumulador += cont;
+  accumulador += "\n";
+}
+console.log(accumulador);
+
+
+
